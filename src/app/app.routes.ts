6f66f5c4135521c4737a3630d64c2e555f7247c0
john_guerrero1 <johnalexguerrero@gmail.com>
2026-04-28 17:26:52 -0500

    init app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/home/pages/dashboard/dashboard';
+import { OldTestament } from './features/old-testament/pages/old-testament/old-testament';
+import { NewTestament } from './features/new-testament/pages/new-testament/new-testament';
+import { Reader } from './features/reader/pages/reader/reader';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+        title:'home'
+    },
+    {
+        path: 'old-testament',
+        component: OldTestament,
+        title: 'Old Testament'
+    },
+    {
+        path: 'old-testament/:book',
+        component: Reader,
+        title: 'reader book Old Testament'
+    },
+    {
+        path: 'new-testament',
+        component: NewTestament,
+        title: 'New Testament'
+    },
+    {
+        path: 'new-testament/:book',
+        component: Reader,
+        title: 'Reader book New Testament'
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Siderbar } from "./layout/siderbar/siderbar";
+import { Navbar } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    Siderbar, Navbar
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
