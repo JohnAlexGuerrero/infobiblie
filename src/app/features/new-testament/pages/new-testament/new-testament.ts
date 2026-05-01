4fc2163cbbd9441e9d665e00decddc1850a8fbdd
john_guerrero1 <johnalexguerrero@gmail.com>
2026-05-01 11:53:00 -0500

    feat init component new-testament

---
+++ src/app/features/new-testament/pages/new-testament/new-testament.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-new-testament',
+  imports: [RouterLink],
+  templateUrl: './new-testament.html',
+  styles: ``,
+})
+export class NewTestament {
+
+}
